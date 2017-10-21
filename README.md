f6207291d9311a84fb3a1e9245cfee385ef6b824
Jinya Chen <jinyachen@gmail.com>
2017-10-21 14:10:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,85 @@
+# 声网Web客户端Demo
+
+官网给的SDK写的很淳朴，导致调试了半天都不对劲，所以自己写了一个Demo，供测试使用。
+
+## 声网 Web API 
+
+点击这里去官网 [Web API - WebRTC ](https://docs.agora.io/cn/user_guide/API/webrtc_interop_api.html) 
+
+## 服务器架设指南
+
+本地搭建简易的 Https Nginx 静态服务器
+
+### 一，生成证书
+```
+# 1、首先，进入你想创建证书和私钥的目录，例如：
+cd /etc/nginx/
+
+# 2、创建服务器私钥，命令会让你输入一个口令：
+openssl genrsa -des3 -out server.key 1024
+
+# 3、创建签名请求的证书（CSR）：
+openssl req -new -key server.key -out server.csr
+
+# 4、在加载SSL支持的Nginx并使用上述私钥时除去必须的口令：
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+
+
+# 5、最后标记证书使用上述私钥和CSR：
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+```
+
+### 二，配置nginx
+
+```
+cd /etc/nginx
+vim nginx.conf
+#
+# HTTPS server configuration
+#
+server {
+    listen       443;
+    server_name  本机的IP地址;
+
+    ssl                  on;
+    ssl_certificate      /etc/nginx/server.crt;
+    ssl_certificate_key  /etc/nginx/server.key;
+
+    ssl_session_timeout  5m;
+
+#    ssl_protocols  SSLv2 SSLv3 TLSv1;
+#    ssl_ciphers  ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP;
+#    ssl_prefer_server_ciphers   on;
+
+    location / {
+        root   html;
+        index  testssl.html index.html index.htm;
+    }
+}
+
+```
+
+## 初始化
+
+1. 需要在 `app.js`里面填写默认的获取ChannelKey的 url地址, 如果没有使用 Dynamic Key，注释掉相关获取key代码即可
+1. 需要在 `index.html`里面填写默认的AppId
+1. 为了测试方便，可在 `index.html` 里面把 Channel Id 和 User Id 都写上默认值
+
+## 注意点
+
+1. 确认Channel Key的生成方式，静态或者动态，参照官网 [密钥用户指南](https://docs.agora.io/cn/user_guide/Component_and_Others/Dynamic_Key_User_Guide.html)
+2. `AgoraRTC`的 `init` 需要 appId ,  `join` 需要channelKey
+3. `stream`的`play`方法传入HTML Element Id，SDK会自动创建DOM
+4. user id 需要是纯数字， channel id 需要是字符串
+
+## 常见错误
+| Name        | Code           | Comment  ||
+| ------------- |:-------------:| -----:|
+| ERR_INVALID_ARGUMENT | 	2	 | API调用了无效的参数。例如指定的频道名含有非法字符。 |一般是参数错误|
+|WARN_LOOKUP_CHANNEL_REJECTED	|105|	查找频道请求被服务器拒绝。服务器可能没有办法处理这个请求或请求是非法的。|一般是channelKey的问题|
+|‘SERVICE_NOT_AVAILABLE’|	-| 服务不可用|一般是appId的问题|
+
+官网错误说明 [错误代码和警告代码](https://docs.agora.io/cn/user_guide/troubleshooting/error.html)
+
+
---
+++ app.js
@@ -0,0 +1,304 @@
+
+var App = function(){
+    this.init();
+}
+App.prototype = {
+    URL: {
+        key: '<key url>'
+    },
+    appId: '',
+    channelKey: null,
+    channelId: '',
+    userId: 0,
+    isHost: false,
+    client: null,
+    localStream: null,
+    audioList: [],
+    videoList: [],
+    $statusWrap: $('[agora-status-check-wrap]'),
+    $audioWrap : $('select[select-audio-source]'),
+    $videoWrap: $('select[select-video-source]'),    
+    appendCheckStatus(word, status = 1){
+
+        let statusClassName = 'alert-success';
+        let statusIconClassName = 'glyphicon-ok'
+        if(status == 1) {
+            statusClassName = 'alert-success';
+            statusIconClassName = 'glyphicon-ok-sign'
+        } else if(status == 2) {
+            statusClassName = 'alert-warning';
+            statusIconClassName = 'glyphicon-exclamation-sign'
+        } else if(status == 3) {
+            statusClassName = 'alert-danger';
+            statusIconClassName = 'glyphicon-remove-sign'
+        }
+
+        let html = '<div class="alert '+ statusClassName +' inline-alert" role="alert">' 
+        + '  <span class="glyphicon '+ statusIconClassName + '" aria-hidden="true"></span>' 
+        + '<span> ' + word + '</span>' + 
+        '</div>';
+        this.$statusWrap.append(html);
+        
+    },
+   
+    init (){
+
+        let app_id = $('input[input-app-id]').val();
+
+        this.appId = app_id ? app_id.toString() : null;
+
+        this.client = AgoraRTC.createClient({
+            mode: 'interop'
+        });
+
+        this.client.init(this.appId, ()=> {
+            console.log("AgoraRTC client initialized");
+            this.appendCheckStatus('AgoraRTC client initialized');
+           
+        },  (err) => {
+            console.log("AgoraRTC client init failed", err);
+            this.appendCheckStatus('AgoraRTC client init failed: ' + err, 3);
+            
+        });
+        this.bindEvent();
+        this.bindClientEvent();
+        this.getDevices();
+    
+       
+    },
+    
+    bindEvent(){
+        $('body').on('click', '[btn-join]', (e)=>{
+            this.join();
+            $(e.currentTarget).attr('disabled', true);
+            $('button[btn-leave]').attr('disabled', null);
+
+            if(this.isHost) {
+                $('button[btn-publish]').attr('disabled', null);
+                $('button[btn-unpublish]').attr('disabled', null);
+            }
+            
+            
+        }).on('click', '[btn-leave]', (e)=>{
+            $(e.currentTarget).attr('disabled', true);
+            $('button[btn-join]').attr('disabled', null);
+            $('button[btn-publish]').attr('disabled', true);
+            $('button[btn-unpublish]').attr('disabled', true);
+
+            this.leave();
+        }).on('click', '[btn-publish]', (e)=>{
+            $(e.currentTarget).attr('disabled', true);
+            $('button[btn-unpublish]').attr('disabled', null);
+
+            this.publish();
+        }).on('click', '[btn-unpublish]', (e)=>{
+            $(e.currentTarget).attr('disabled', true);
+            $('button[btn-publish]').attr('disabled', null);
+            this.unpublish();
+        })
+    },
+    bindClientEvent(){
+         // channelKey = "";
+        let client = this.client;
+        client.on('error', (err)  => {
+            console.log("Got error msg:", err.reason);
+            if (err.reason === 'DYNAMIC_KEY_TIMEOUT') {
+                this.getChannelKey(()=>{
+                    this.client.renewChannelKey(channelKey, function(){
+                        console.log("Renew channel key successfully");
+                    }, function(err){
+                        console.log("Renew channel key failed: ", err);
+                    });
+                });
+               
+            }
+        });
+    
+        client.on('stream-added', function (evt) {
+            let stream = evt.stream;
+            console.log("New stream added: " + stream.getId());
+            console.log("Subscribe ", stream);
+            client.subscribe(stream, function (err) {
+                console.log("Subscribe stream failed", err);
+            });
+        });
+    
+        client.on('stream-subscribed', function (evt) {
+            let stream = evt.stream;
+            console.log("Subscribe remote stream successfully: " + stream.getId());
+            if ($('#video_remote_'+stream.getId()).length === 0) {
+                $('[video-remote-wrap]').append('<div class="video-remote" id="video_remote_'+stream.getId()+'" "></div>');
+            }
+            stream.play('video_remote_' + stream.getId());
+        });
+    
+        client.on('stream-removed', function (evt) {
+            let stream = evt.stream;
+            stream.stop();
+            $('#video_remote_'+stream.getId()).remove();
+            console.log("Remote stream is removed " + stream.getId());
+        });
+    
+        client.on('peer-leave', function (evt) {
+            let stream = evt.stream;
+            if (stream) {
+                stream.stop();
+                $('#video_remote_'+stream.getId()).remove();
+                console.log(evt.uid + " leaved from this channel");
+            }
+        });
+    },
+    getDevices () {
+        AgoraRTC.getDevices((devices) => {
+            for (let i = 0; i !== devices.length; ++i) {
+                let device = devices[i];
+                if (device.kind === 'audioinput') {
+                    this.audioList.push({
+                        text: device.label || 'microphone ' + (this.audioList.length + 1),
+                        value: device.deviceId
+                    });
+                    
+                } else if (device.kind === 'videoinput') {
+                    this.videoList.push({
+                        text: device.label || 'camera ' + (this.videoList.length + 1),
+                        value: device.deviceId
+                    });
+                } else {
+                    console.log('Some other kind of source/device: ', device);
+                }
+            }
+
+            if(this.audioList.length) {
+                this.appendCheckStatus('Audio device ok');
+                for(let v of this.audioList) {
+                    this.$audioWrap.append('<option value="'+ v.value +'" >' + v.text+ '</option>');
+                }
+            } else {
+                this.appendCheckStatus('Audio device disabled', 2);
+            }
+
+            if(this.videoList.length) {
+                this.appendCheckStatus('Video device ok');
+                for(let v of this.videoList) {
+                    this.$videoWrap.append('<option value="'+ v.value +'" >' + v.text+ '</option>');
+                }
+            } else {
+                this.appendCheckStatus('Audio device disabled', 2);
+            }
+        });
+    },
+    setup (callback){
+        
+        let channel_id = $('input[input-channel-id]').val();
+        let user_id = $('input[input-user-id]').val();
+        let is_host = $('input[checkbox-is-host]')[0].checked;
+
+        this.isHost = is_host;
+        this.channelId = channel_id ? channel_id.toString() : null;
+        this.userId = user_id ? parseInt(user_id) : null;
+
+        if(this.channelId) {
+            this.appendCheckStatus('Channel Id ok');
+        } else {
+            this.appendCheckStatus('Channel Id is empty ', 2);
+        }
+
+        if(this.userId) {
+            this.appendCheckStatus('User Id  ok');
+        } else {
+            this.appendCheckStatus('User Id is empty', 2);
+        }
+
+        if(this.appId) {
+            this.appendCheckStatus('App Id ok');
+        } else {
+            this.appendCheckStatus('App Id is empty', 2);
+        }
+
+        this.getChannelKey(callback);
+    },
+    getChannelKey (callback) {
+        $.get(this.URL.key, {
+            'channel_id': this.channelId,
+            'user_id': this.userId,
+        }, (ret) => {
+            if(ret && ret.errno == 0) {
+                this.channelKey = ret.data.channel_key
+                if(this.channelKey) {
+                    this.appendCheckStatus('channel key = ' + this.channelKey);
+                } else {
+                    this.appendCheckStatus('channel key = ' + this.channelKey, 3);
+                }
+                callback && callback();
+            } else {
+                this.appendCheckStatus('channel key init error', 3);
+                // alert(JSON.stringify(ret));
+            }
+        });
+    },
+    join (){
+        this.setup(()=>{
+            this.client.join(this.channelKey, this.channelId, this.userId, (streamID) => {
+                console.log("User " + streamID + " join channel successfully");
+                this.appendCheckStatus("Join channel successfully");
+
+                if ($('input[checkbox-is-host]')[0].checked) {
+                    let camera = this.$videoWrap.val();
+                    let microphone = this.$audioWrap.val();
+                    console.log('AgoraRTC.createStream');
+                    let param = {
+                        streamID: streamID, 
+                        audio: true, 
+                        cameraId: camera, 
+                        microphoneId: microphone, 
+                        video: true, 
+                        screen: false
+                    }
+                    this.localStream = AgoraRTC.createStream(param);
+                    this.localStream.setVideoProfile('320p_3');
+                    this.localStream.init(()=> {
+                        console.log("getUserMedia successfully");
+                        this.localStream.play('video_local');
+                        this.client.publish(this.localStream, (err) => {
+                            console.log("Publish local stream error: " + err);
+                        });
+    
+                        this.client.on('stream-published', (evt) => {
+                            console.log("Publish local stream successfully");
+                        });
+                    }, (err) => {
+                        console.log("getUserMedia failed", err);
+                    });
+                } else {
+                    this.appendCheckStatus("not Host", 2);
+                }
+            }, function(err) {
+                console.log("Join channel failed", err);
+            });
+        });
+        
+    },
+    leave() {
+        this.client.leave(function () {
+            console.log("Leavel channel successfully");
+        }, function (err) {
+            console.log("Leave channel failed");
+        });
+    },
+    unpublish() {
+        this.client.unpublish(this.localStream,  (err) => {
+          console.log("Unpublish local stream failed" + err);
+        });
+    },
+    publish() {
+        this.client.publish(this.localStream, function (err) {
+            console.log("Publish local stream error: " + err);
+        });
+    }
+      
+}
+
+
+$(document).ready(function(){
+    const app = new App();
+});
